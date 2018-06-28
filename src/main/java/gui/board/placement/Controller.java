3733fa5804b9bea67560c0fd4a14455ee2ce03e3
AdrianHarenczyk <adrian.harenczyk@gmail.com>
2018-06-28 16:11:49 +0200

    Change placeRandom method and added ship sizes

--- src/main/java/gui/board/placement/Controller.java
+++ src/main/java/gui/board/placement/Controller.java
@@ -7,14 +7,17 @@ import javafx.scene.shape.Rectangle;
 
 public class Controller {
 
+    private static final int BATTLESHIP = 4;
+    private static final int CRUISER = 3;
+    private static final int DESTROYER = 2;
+    private static final int SUBMARINE = 1;
+
     @FXML
     private GridPane board;
 
     @FXML
     private void placeRandom() {
-        for (int i = 0; i < 10; i++) {
-            board.add(createRectangle(),i,i);
-        }
+        createShip(BATTLESHIP,1,2,true);
     }
 
     private Rectangle createRectangle() {
@@ -22,5 +25,14 @@ public class Controller {
         rectangle.setHeight(50);
         rectangle.setWidth(50);
         return rectangle;
+    }
+
+    private void createShip(int shipType, int columnPosition, int rowPosition, boolean horizontal) {
+        if (horizontal) {
+            for (int i = 0; i < shipType; i++) {
+                board.add(createRectangle(),rowPosition,columnPosition);
+                rowPosition++;
+            }
+        }
     }
 }
