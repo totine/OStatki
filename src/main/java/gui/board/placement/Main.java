91deb9915ce3ccbde6a4ea97b53c347ded1fc886
AdrianHarenczyk <adrian.harenczyk@gmail.com>
2018-06-28 15:20:55 +0200

    Change structure of project, fxmls in resources

--- src/main/java/gui/board/placement/Main.java
+++ src/main/java/gui/board/placement/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
         primaryStage.setTitle("Battleships OStatki");
         primaryStage.setScene(new Scene(root, 1200, 900));
         primaryStage.show();
